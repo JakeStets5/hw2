a34f4a834c444ed1cec2e07a44bd0a576cf2fa76
Brian Carlos <briancarlos@eos.beocat.ksu.edu>
2024-02-08 10:22:19 -0600

    added test

---
+++ test/tests.cpp
@@ -0,0 +1,47 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include "gtest/gtest.h"
+#include <pthread.h>
+#include "../include/processing_scheduling.h"
+
+// Using a C library requires extern "C" to prevent function managling
+extern "C" 
+{
+#include <dyn_array.h>
+}
+
+
+#define NUM_PCB 30
+#define QUANTUM 5 // Used for Robin Round for process as the run time limit
+
+unsigned int score;
+unsigned int total;
+
+class GradeEnvironment : public testing::Environment 
+{
+    public:
+        virtual void SetUp() 
+        {
+            score = 0;
+            total = 210;
+        }
+
+        virtual void TearDown()
+        {
+            ::testing::Test::RecordProperty("points_given", score);
+            ::testing::Test::RecordProperty("points_total", total);
+            std::cout << "SCORE: " << score << '/' << total << std::endl;
+        }
+};
+
+TEST(load_process_control_blocks, NullFileName) {
+    const char *input_filename = NULL;
+    EXPECT_EQ(NULL, load_process_control_blocks(input_filename));
+}
+
+int main(int argc, char **argv) 
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::AddGlobalTestEnvironment(new GradeEnvironment);
+    return RUN_ALL_TESTS();
+}
